5878edea5b867920499027b8108e90fc186a77b7
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2013-11-22 18:17:00 +0100

    reconnect fixed

--- server.js
+++ server.js
@@ -6,41 +6,65 @@ var findconfig = new require('./lib/findconfig');
 var logger = new loggerLib();
 var config;
 var socket;
+var initial_connection = true;
 
 function init(config,path){
+	
 	socket = io.connect(config.collectServer,{
 		port: config.collectPort
 	});
+	//io.set('log level', config.loglevel*1);
+	
+	
+	socket.on('error', function(err){
+		if (err.indexOf('ECONNREFUSED')){
+			logger.log('error','The crossmon-server is not available');
+			//setTimeout(init,6000,config,path);
+		}else{
+			logger.log('error',err);
+			//console.log(err);
+		}
+	});
+	
+	
+	socket.on('disconnect', function(){
+		logger.log('error','lost connection');
+	});
 	
 	socket.on('connect', function () {
-		for(var i in config.collect){
-			if (typeof config.collect[i].enabled=='undefined'){
-				config.collect[i].enabled=true; // default: any module is enabled
-			}
-			if (config.collect[i].enabled===true){
-				logger.log('info','initialize collector: '+config.collect[i].module);
-				try{
-					var module = require(config.collect[i].module);
-					if (typeof module.mininterval=='undefined'){
-						module.mininterval = 1000; 
+		logger.log('info','connection established');
+		if (initial_connection){
+			logger.log('info','starting modules');
+			for(var i in config.collect){
+				if (typeof config.collect[i].enabled=='undefined'){
+					config.collect[i].enabled=true; // default: any module is enabled
+				}
+				if (config.collect[i].enabled===true){
+					logger.log('info','initialize collector: '+config.collect[i].module);
+					try{
+						var module = require(config.collect[i].module);
+						if (typeof module.mininterval=='undefined'){
+							module.mininterval = 1000; 
+						}
+						
+						// initial monitoring
+						if (Math.max(config.collect[i].interval,module.mininterval)>10000){
+							require(config.collect[i].module).monitor(socket,config.collect[i].options);
+						}
+						
+						var interval = setInterval(
+							require(config.collect[i].module).monitor, 
+							Math.max(config.collect[i].interval,module.mininterval), 
+							socket,
+							config.collect[i].options
+						);
+						
+					}catch(e){
+						logger.log('error',e);
 					}
-					
-					// initial monitoring
-					if (Math.max(config.collect[i].interval,module.mininterval)>10000){
-						require(config.collect[i].module).monitor(socket,config.collect[i].options);
-					}
-					
-					var interval = setInterval(
-						require(config.collect[i].module).monitor, 
-						Math.max(config.collect[i].interval,module.mininterval), 
-						socket,
-						config.collect[i].options
-					);
-					
-				}catch(e){
-					logger.log('error',e);
 				}
 			}
+			initial_connection=false;
 		}
 	});
 }
